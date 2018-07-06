1d4bab081dc645c19ed0c700043c3f75921ab321
Bolubash Pavel <5avel@hotmail.com>
2018-07-06 11:17:04 +0300

    Init

---
+++ DataService/Model/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataService.Model
+{
+    public class User
+    {
+        public int Id { set; get; }
+        public DateTime CreatedAt { set; get; }
+        public string Name { set; get; }
+        public string Avatar { set; get; }
+        public string Email { set; get; }
+        public IEnumerable<Post> Posts { get; set; }
+        public IEnumerable<Todo> Todos { get; set; }
+        public IEnumerable<Addres> Address { get; set; }
+    }
+}
